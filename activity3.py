1e4761a8b07ec63c96d9e5fefe536d828e63c1db
razaiqbal2 <duaf0226@gmail.com>
2025-01-18 08:47:12 +0500

    turtle multicolor

---
+++ activity3.py
@@ -0,0 +1,25 @@
+import turtle
+
+t=turtle.Turtle()
+s=turtle.Screen()
+turtle.Screen()
+
+colors = ['red','blue','yellow','purple','orange','gold']
+s.bgcolor('black')
+t.speed('fastest')
+t.hideturtle()
+while True:
+    for x in range(250) :
+        t.pencolor(colors[x%len(colors)])
+        t.width(x/100 + 1)
+        t.forward(x)
+        t.left(59)
+    t.right(239)
+    for x in range(200 , 0 ,-1):
+        t.pencolor('black')
+        t.width(x /100 + 7)
+        t.forward(x)
+        t.right(59)
+
+
+
